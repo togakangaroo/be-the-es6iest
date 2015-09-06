15b39ea3d92dfe9057c59e770944cd256ea82dce
George Mauer <gmauer@gmail.com>
2015-09-06 13:21:46 -0500

    store notifications and respond and use a form on ui

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-import coroutine from 'coroutine'
-
 //Things to demo:
 // [ ]	simple object construction
 // [a]	object destructuring in parameters
@@ -11,41 +9,61 @@ import coroutine from 'coroutine'
 // [a]	default parameters
 // [ ]	first order functions
 // [a]	hoisting 
-// [ ]	json objects
+// [d]	json objects
 // [b]	splat
 // [b]	indexor syntax
 // [a]	modules
-// [a]	Generators
+// [ ]	Generators
 //
 // a - initial
 // b - add extend, remove query
 // c - get rid of button
+// d - no generators, objects, form
 
 const createElement = (tag='div', textContent = "") => extend(document.createElement(tag), {textContent})
 const toggleClass = (el, addRemove, ...classNames) => el.classList[addRemove](...classNames)
 
-const createNotificationQueue = coroutine(function*({toKeep = 5}={}) {
-	const queue = [];
+const createNotificationQueue = function({toKeep = 5}={}) {
+	let queue = [];
 	const notificationsContainer = createElement('ol');
 	document.body.appendChild(notificationsContainer)
 	toggleClass(notificationsContainer, 'add', 'notifications', 'hidden')
 
-	while(true) {
-		queue.push(yield)
-		if(queue.length > toKeep)
-			queue.shift();
-		showQueue(notificationsContainer, queue);
-	}
+	return {
+		add(msg) {
+			queue = [...queue, msg];
+			if(queue.length > toKeep)
+				queue = queue.slice(1);
+			showQueue(notificationsContainer, queue);
+		},
+		clear() {
+			queue = [];
+			showQueue(notificationsContainer, queue);
+		},
+	};
+};
 
-});
-
-const resultsPrinter = createNotificationQueue()
-document.querySelector('input').addEventListener('keyup', ({which, target: {value}}) => {
-	if(which == 13)
-		resultsPrinter.next(value)
-})
+const notifications = createNotificationQueue({toKeep: 3})
+let counter = 1;
+on('form', 'submit', preventDefault((e) => {
+	const value = e.target.querySelector('input').value
+	notifications.add(`${counter++}: ${value}`)	
+}))
+on('form', 'reset', preventDefault(() => notifications.clear()) )
 
 /////////////////////////////////////////////////////
+
+function preventDefault(fn) { return (e, ...args) => {
+	e.preventDefault();
+	return fn(e, ...args);
+} }
+
+function on(selector, eventType, fn) {
+	const el = document.querySelector(selector);
+	el.addEventListener(eventType, fn);
+	return el;
+}
+
 function extend(...args) {
 	const [first, second, ...rest] = args;
 	if(!second && !rest.length) return first;
@@ -60,3 +78,5 @@ function showQueue(notificationsContainer, queue) {
 	toggleClass(notificationsContainer, 'remove', 'hidden')
 	setTimeout(()=>toggleClass(notificationsContainer, 'add', 'hidden'), 10);
 }
+
+export default null //without this file is not interpreted as a module
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var exec = require('child_process').exec;
 var app = express();
 
 app.use('/', express.static('public'));
+app.use(bodyParser.json());
+
+var storedNotifications = [];
+app.post('/notifications', function(req, resp) {
+	storedNotifications = req.body;
+	console.log("stored", storedNotifications);	
+	resp.status(200).end(); 
+});
+app.get('/notifications', function(req, resp) {	
+	console.log("responding with", storedNotifications)
+	resp.json(storedNotifications);
+});
 
 var server = app.listen(8337, function () {
   var host = server.address().address;
