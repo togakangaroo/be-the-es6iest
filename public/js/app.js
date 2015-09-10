9f3a395f1d187041135ebe4c7789b584d46c51dc
George Mauer <gmauer@gmail.com>
2015-09-09 23:08:01 -0500

    appjs cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,20 @@
 import createNotificationQueue from 'createNotificationQueue'
+import store from 'customNotificationsStore'
 
 const notifications = createNotificationQueue()
 
-let counter = 1;
+let counter = 0;
 const form = document.querySelector('form')
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const value = e.target.querySelector('input').value;
 	counter += 1;
+
 	notifications.add(`${counter}: ${value}`);
 });
 form.addEventListener('reset', (e) => {
 	e.preventDefault();
+
 	notifications.clear();
 })
