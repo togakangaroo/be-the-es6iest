cecce20c64f1a6b19e50e1373f72f04340599ad7
George Mauer <gmauer@gmail.com>
2015-09-10 20:05:05 -0500

    step 2

--- public/js/createNotificationQueue.js
+++ public/js/createNotificationQueue.js
@@ -1,13 +1,27 @@
 
 const createNotificationQueue = () => {
+	let queue = []
+
 	return {
 		clear() {
+			queue = []
 
+			showNotifications()
 		},
 		add(msg) {
-			
+			queue = [...queue, msg]
+			if(queue.length > 5)
+				queue = queue.slice(1)
+
+			showNotifications()
 		}
 	}
+
+	//////////////////////////////////
+	function showNotifications() {
+		console.log("Current notifications:", queue)
+	}
+
 }
 
 export default createNotificationQueue
